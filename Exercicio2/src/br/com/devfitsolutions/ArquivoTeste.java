83dd734b7c0930569343f83e80e841463b87f2e8
victor <victor@CO-TI-02.redebarbosa.local>
2015-11-17 11:31:19 -0200

    teste consoante

--- Exercicio2/src/br/com/devfitsolutions/ArquivoTeste.java
+++ Exercicio2/src/br/com/devfitsolutions/ArquivoTeste.java
@@ -8,7 +8,7 @@ import java.io.InputStreamReader;
 public class ArquivoTeste {
 
 	public static void main(String[] args) throws IOException{
-	    FileInputStream entrada = new FileInputStream("/Users/victorvianasantos/Downloads/Hirens/teste.txt");
+	    FileInputStream entrada = new FileInputStream("C:\\temp\\teste.txt");
 	    InputStreamReader entradaFormatada = new InputStreamReader(entrada);
 	    int caracteres = entradaFormatada.read();
 	    //int consoantes = entradaFormatada.read();
@@ -16,7 +16,7 @@ public class ArquivoTeste {
 	    int consoantes = 0;
 	    int vogais = 0, cont = 0, espaco =0, linhas = 0, enter =0;
 	    
-	    while( caracteres!=-1){
+	    while( caracteres != -1){
 	    	System.out.print( (char)caracteres);
 	    	caracteres = entradaFormatada.read();
 	    	if (caracteres == 97 || caracteres == 101 || caracteres == 105 || caracteres == 111 || caracteres == 117){
@@ -25,7 +25,7 @@ public class ArquivoTeste {
 	    	if (caracteres == 32){
 	    		espaco++;
 	    	}
-	    	if (caracteres == 0){
+	    	if (caracteres > 33 && caracteres <= 64 ){
 	    		linhas++;
 	    	}
 	    	/*if (caracteres == 32 || caracteres == 13){
@@ -39,7 +39,9 @@ public class ArquivoTeste {
 	    
 	    consoantes = cont - vogais;
 	    //palavra++;
+	    linhas++;
 	    cont = cont - espaco - enter;
+	    consoantes = cont - espaco - linhas - vogais - 1;
 	    System.out.printf("\nvogais: " +vogais+ "\n");
 	    System.out.println("consoantes: " +consoantes);
 	    System.out.println("espacos: " +espaco);
